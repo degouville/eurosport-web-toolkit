436e985a3d05269d8cbe4abf1ae9f1ee580ff1bc
David G <davidgarry@users.noreply.github.com>
2019-05-15 17:44:22 +0200

    Update webhook url (#146)

--- .circleci/deploymentNotification.js
+++ .circleci/deploymentNotification.js
@@ -4,7 +4,7 @@ const { version } = require('../package.json');
 
 (function() {
   return axios
-    .post('https://hooks.slack.com/services/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', {
+    .post('https://hooks.slack.com/services/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', {
       username: 'Deployment notification',
       icon_url:
         'https://www.beyondanxietyanddepression.com/sites/beyondanxietyanddepression.com/files/styles/large/public/how-stop-panic-attacks-step-step.jpg',
