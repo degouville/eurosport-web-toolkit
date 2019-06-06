3c3d76019b953d3137dfd9158d22de9df239d2f2
Saad <saad-kh@users.noreply.github.com>
2019-06-06 13:54:48 +0200

    Double Export strategy ESM and CJS (#216)
    * Double Export strategy ESM and CJS
    
    * fixing js-yaml vulnerability
    
    * Revert "fixing js-yaml vulnerability"
    
    This reverts commit daa5edd1304abc3286187ffe5d38ed64050dd384.

---
+++ .babelrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: ['@babel/preset-react', ['@babel/preset-env', { modules: process.env.MODULE ? false : 'commonjs' }]],
+  plugins: [
+    'emotion',
+    '@babel/plugin-proposal-class-properties',
+    'react-docgen',
+    [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+        },
+      },
+    ],
+    ['inline-react-svg', { ignorePattern: '(?<!\\.component)\\.svg$' }],
+    [
+      'file-loader',
+      {
+        name: '[hash].[ext]',
+        extensions: ['png', 'jpg', 'jpeg', 'gif', 'svg'],
+        publicPath: '/eurosport-web-toolkit/',
+        outputPath: '/dist/assets',
+        context: 'src',
+        limit: 0,
+      },
+    ],
+  ],
+};
