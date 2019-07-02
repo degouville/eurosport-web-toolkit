6ac9731f692a3287be24d4e7b3575468d0aeade0
Raphael Roullet <raphael_roullet.external@discovery.com>
2019-07-02 17:03:58 +0200

    Improve responsiveness

--- src/elements/ProgramDetails/index.js
+++ src/elements/ProgramDetails/index.js
@@ -1,62 +1,69 @@
 import React from 'react';
-import styled from 'react-emotion';
+import styled, { css } from 'react-emotion';
 import PropTypes from 'prop-types';
 import ChannelIcon from '../ChannelIcon';
 import { suitGray } from '../../colors';
+import { medium } from '../../breakpoints';
 
-const StyledDetails = styled.div`
+export const StyledDetails = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
   height: 100%;
   border-top: 1px solid rgba(255, 255, 255, 0.15);
   color: ${suitGray};
-  * :nth-child(2) {
-    margin-left: 10px;
-    padding-left: 10px;
+  font-size: 11px;
 
-    padding: 8px;
+  svg {
+    width: auto;
+    height: 100%;
   }
+
+  ${medium(css`
+    font-size: 14px;
+    line-height: 20px;
+  `)};
 `;
-const StyledIconWrapper = styled.div`
-  padding: ${props => `${props.iconPadding}px`};
+
+export const StyledIconWrapper = styled.div`
+  padding: 10px;
   padding-left: 0;
   border-right: 1px solid rgba(255, 255, 255, 0.15);
+  height: 15px;
+
+  ${medium(css`
+    padding: 15px;
+    padding-left: 0;
+    height: 23px;
+  `)};
 `;
 
-const StyledText = styled.div`
-  ${props =>
-    `font-size: ${props.fontSize}px;
-    padding: ${props.textPadding}px 0;
-  `};
+export const StyledText = styled.div`
+  padding: 10px;
+
+  ${medium(css`
+    padding: 15px;
+  `)};
 `;
 
-const ProgramDetails = ({ callsign, textDetail, iconHeight, padding, fontSize }) => (
+const ProgramDetails = ({ callsign, textDetail }) => (
   <StyledDetails>
     {callsign && (
-      <StyledIconWrapper iconPadding={padding}>
-        <ChannelIcon type={callsign} height={iconHeight} />
+      <StyledIconWrapper>
+        <ChannelIcon type={callsign} />
       </StyledIconWrapper>
     )}
-    <StyledText fontSize={fontSize} textPadding={padding}>
-      {textDetail}
-    </StyledText>
+    <StyledText>{textDetail}</StyledText>
   </StyledDetails>
 );
 
 ProgramDetails.defaultProps = {
   callsign: 'E',
-  iconHeight: 23,
-  padding: 15,
-  fontSize: 14,
 };
 
 ProgramDetails.propTypes = {
   callsign: PropTypes.string,
   textDetail: PropTypes.string.isRequired,
-  iconHeight: PropTypes.number,
-  padding: PropTypes.number,
-  fontSize: PropTypes.number,
 };
 
 export default ProgramDetails;
--- src/elements/ProgramDetails/index.stories.js
+++ src/elements/ProgramDetails/index.stories.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, number } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
 import ProgramDetails from '.';
 
 const ProgramDetailsStories = storiesOf('Elements|ProgramDetails', module);
 
-ProgramDetailsStories.add('configurable', () => (
-  <ProgramDetails
-    callsign={text('callsign', 'E')}
-    textDetail={text('textDetail', '09:00 - 10:00')}
-    iconHeight={number('iconHeight', 23)}
-    padding={number('padding', 15)}
-    fontSize={number('fontSize', 16)}
-  />
+ProgramDetailsStories.add('default', () => (
+  <ProgramDetails callsign={text('callsign', 'E')} textDetail={text('textDetail', '09:00 - 10:00')} />
 ));
