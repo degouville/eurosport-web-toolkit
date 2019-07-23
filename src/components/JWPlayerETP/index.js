ce557cffb7fd85d182c2a77c1e3f742fc78b6367
Saad <saad-kh@users.noreply.github.com>
2019-07-23 10:12:09 +0200

    updating ETP to 0.3.1 (#269)

--- src/components/JWPlayerETP/index.js
+++ src/components/JWPlayerETP/index.js
@@ -2,26 +2,107 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ETPlayerComponent from '@eurosport/toolkit-player';
 
-const ETPlayer = ({ prefLang, videoData, jwplayerId, elementId }) => (
-  <ETPlayerComponent locale={prefLang} videoData={videoData} jwplayerId={jwplayerId} elementId={elementId} />
+const ETPlayer = ({
+  prefLang,
+  elementId,
+  jwplayerData,
+  videoData,
+  onReady,
+  onPlay,
+  onPause,
+  onAdBreakStart,
+  onAdBreakComplete,
+  onAdStart,
+  onAdTime,
+  onError,
+  onLoginModalReady,
+  onLoginModalDone,
+}) => (
+  <ETPlayerComponent
+    locale={prefLang}
+    elementId={elementId}
+    jwplayerData={jwplayerData}
+    videoData={videoData}
+    onReady={onReady}
+    onPlay={onPlay}
+    onPause={onPause}
+    onAdBreakStart={onAdBreakStart}
+    onAdBreakComplete={onAdBreakComplete}
+    onAdStart={onAdStart}
+    onAdTime={onAdTime}
+    onError={onError}
+    onLoginModalReady={onLoginModalReady}
+    onLoginModalDone={onLoginModalDone}
+  />
 );
 
 ETPlayer.defaultProps = {
-  jwplayerId: undefined,
+  onReady: null,
+  onPlay: null,
+  onPause: null,
+  onAdBreakStart: null,
+  onAdBreakComplete: null,
+  onAdStart: null,
+  onAdTime: null,
+  onError: null,
+  onLoginModalReady: null,
+  onLoginModalDone: null,
 };
 
 ETPlayer.propTypes = {
   prefLang: PropTypes.string.isRequired,
   elementId: PropTypes.string.isRequired,
-  jwplayerId: PropTypes.string,
+
+  jwplayerData: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    key: PropTypes.string.isRequired,
+  }).isRequired,
+
   videoData: PropTypes.shape({
     provider: PropTypes.string.isRequired,
-    videoId: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string,
+    title: PropTypes.string,
+    duration: PropTypes.number,
+    type: PropTypes.string,
+
     sonic: PropTypes.shape({
       baseUrl: PropTypes.string.isRequired,
       realm: PropTypes.string.isRequired,
     }),
+
+    mux: PropTypes.shape({
+      env_key: PropTypes.string.isRequired,
+    }),
+
+    freewheelAdParams: PropTypes.shape({
+      adManagerURL: PropTypes.string.isRequired,
+      serverURL: PropTypes.string.isRequired,
+      networkID: PropTypes.number.isRequired,
+      fwassetId: PropTypes.string,
+      profileId: PropTypes.string,
+      sectionId: PropTypes.string,
+      video_targeting: PropTypes.string,
+      auth: PropTypes.string,
+      sport: PropTypes.string,
+      afid: PropTypes.string,
+      vdur: PropTypes.number,
+      _fw_gdpr: PropTypes.number,
+      _fw_gdpr_consent: PropTypes.string,
+    }),
   }).isRequired,
+
+  // Events
+  onReady: PropTypes.func,
+  onPlay: PropTypes.func,
+  onPause: PropTypes.func,
+  onAdBreakStart: PropTypes.func,
+  onAdBreakComplete: PropTypes.func,
+  onAdStart: PropTypes.func,
+  onAdTime: PropTypes.func,
+  onError: PropTypes.func,
+  onLoginModalReady: PropTypes.func,
+  onLoginModalDone: PropTypes.func,
 };
 
 export default ETPlayer;
--- src/components/JWPlayerETP/index.stories.js
+++ src/components/JWPlayerETP/index.stories.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text } from '@storybook/addon-knobs';
+import { actions } from '@storybook/addon-actions';
 import ETPlayer from './index';
 
-const getBaseProps = () => ({
-  jwplayerId: text('ScriptId (jwplayerId)', 'YWx051uC'),
-  elementId: text('elementId', 'ETPlayer'),
-  prefLang: text('PrefLang', 'fr'),
-  subscribeUrl: text('SubscribeUrl', 'https://www.eurosportplayer.com/subscribe'),
-  videoData: {
-    provider: 'sonic',
-    videoId: text('videoId', 'eurosport-e14611684c0ch0'),
-    sonic: {
-      baseUrl: text('sonicBaseUrl', 'https://eu1-test.disco-api.com'),
-      realm: text('sonicRealm', 'eurosport'),
+const events = actions(
+  'onPlayerInstantiation',
+  'onReady',
+  'onPlay',
+  'onPause',
+  'onAdBreakStart',
+  'onAdBreakComplete',
+  'onAdStart',
+  'onAdTime',
+  'onError',
+  'onLoginModalReady',
+  'onLoginModalDone'
+);
+
+const getBaseProps = () => {
+  const jwplayerId = text('ScriptId (jwplayerId)', 'DWNosgcz');
+  return {
+    prefLang: text('PrefLang', 'fr'),
+    elementId: text('elementId', 'ETPlayer'),
+    jwplayerData: { id: jwplayerId },
+    videoData: {
+      provider: 'sonic',
+      id: text('videoId', 'eurosport-e14558529c9717131ch3'),
+      sonic: {
+        baseUrl: text('sonicBaseUrl', 'https://eu1-test.disco-api.com'),
+        realm: text('sonicRealm', 'eurosport'),
+      },
     },
-  },
-});
+    ...events,
+  };
+};
 
 const stories = storiesOf('Components|JWPlayerETP', module);
 
