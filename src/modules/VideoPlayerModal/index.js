03782ed264a58b39cb78555b859fc3572b5d1a52
Saad <saad-kh@users.noreply.github.com>
2019-07-26 17:43:42 +0200

    Video Player Modal is not fixed (#286)
    * Modal is not fixed
    
    * updating snaps

--- src/modules/VideoPlayerModal/index.js
+++ src/modules/VideoPlayerModal/index.js
@@ -18,11 +18,8 @@ export const MainContainer = styled.div`
   flex: 1;
   align-items: center;
   justify-content: center;
-  position: fixed;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
+  width: 100%;
+  height: 100%;
 `;
 
 export const ChildContainer = styled.div`
