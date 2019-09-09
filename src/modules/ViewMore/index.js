7b2c231e7b63e212d8a072f00bded8eb9b758e38
Mathieu de Gouville <mathieu@degouville.com>
2019-09-09 10:33:57 +0200

    EWL-1013 improve viewmore height refresh (#361)
    * Create story to reproduce the bug
    
    * fix
    
    * Add sizeMe package
    
    * add sizeMe to viewmore
    
    * FIX animation
    
    * U Snapshot
    
    * Improve viewmore sizing
    
    * U snapshot
    
    * Add important todo
    
    * Improve transition
    
    * U snapshot
    
    * U snapshot

--- src/modules/ViewMore/index.js
+++ src/modules/ViewMore/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'react-emotion';
 import { rgba } from 'polished';
-import get from 'lodash/get';
-import throttle from 'lodash/throttle';
+import { throttle, get } from 'lodash';
+import { SizeMe } from 'react-sizeme';
 import { coreLightMinus1, coreLightBase, coreDarkPlus1 } from '../../colors';
 import { fontAlphaHeadlineBold } from '../../typography';
 
@@ -25,19 +25,24 @@ export const StyledViewMoreButton = styled.span`
 `;
 
 const StyledList = styled.ul`
-  overflow: hidden;
-  ${props =>
+  ${({ blockHeight, animate, isDelayGone, animationDelay }) =>
     css`
-      height: ${props.blockHeight !== 0 ? `${props.blockHeight - 2}px;` : 'auto;'}
-      transition: ${props.animate ? 'height 400ms ease;' : 'none;'}
+      max-height: ${blockHeight !== 0 ? `${blockHeight}px` : 'auto'};
+      transition: ${animate ? `max-height ${animationDelay}ms ease` : 'none'};
+      overflow: ${isDelayGone ? 'unset' : 'hidden'};
+
+      // NOTE: <SizeMe> required to have an extra height to render properly
+      border-top: 1em solid transparent;
+      margin-top: -1em;
     `}
 `;
 
-export default class ViewMore extends React.Component {
+class ViewMore extends React.Component {
   listRef = React.createRef();
 
   state = {
     animate: true,
+    isDelayGone: false,
   };
 
   handleWindowResize = throttle(() => this.setState({ animate: false }), 250);
@@ -56,46 +61,71 @@ export default class ViewMore extends React.Component {
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.handleWindowResize);
+    clearInterval(this.delayTimer);
   }
 
-  computeBlockHeight() {
+  computeBlockHeight(height) {
     const { showLessItemCount } = this.props;
     const { expanded } = this.state;
     const listElement = get(this, 'listRef.current', {});
     const listChildren = get(listElement, 'children', []);
+    const shouldExpand = expanded || showLessItemCount >= listChildren?.length;
+    const expandedSize = height;
+    const unExpandedSize = listChildren[showLessItemCount]?.offsetTop;
+    const hasChildren = listChildren?.length > 0;
+    const currentSize = shouldExpand ? expandedSize : unExpandedSize;
+    return hasChildren ? currentSize : 0;
+  }
 
-    let height = 0;
-    if (listChildren.length > 0)
-      height =
-        expanded || showLessItemCount >= listChildren.length
-          ? listElement.scrollHeight
-          : listChildren[showLessItemCount].offsetTop - listElement.offsetTop;
-
-    return height;
+  handleDelay() {
+    this.setState(({ isDelayGone }) => ({ isDelayGone: !isDelayGone }));
   }
 
   handleClick() {
-    this.setState(state => ({
-      expanded: !state.expanded,
+    const { animationDelay } = this.props;
+    const { isDelayGone, animate } = this.state;
+
+    this.setState(({ expanded }) => ({
+      expanded: !expanded,
       animate: true,
     }));
+
+    if (isDelayGone) {
+      this.setState({ isDelayGone: !isDelayGone });
+    } else {
+      this.setState({ animate: !animate });
+      this.delayTimer = setTimeout(() => this.handleDelay(), animationDelay);
+    }
+  }
+
+  renderStyledList(height) {
+    const { children, animationDelay } = this.props;
+    const { expanded, animate, isDelayGone } = this.state;
+
+    return (
+      <StyledList
+        isExpanded={expanded}
+        innerRef={this.listRef}
+        blockHeight={this.computeBlockHeight(height)}
+        animate={animate}
+        isDelayGone={isDelayGone}
+        animationDelay={animationDelay}
+      >
+        {children}
+      </StyledList>
+    );
   }
 
   render() {
     const { children, showLessText, showMoreText, showLessItemCount } = this.props;
-    const { expanded, animate } = this.state;
+    const { expanded } = this.state;
     if (!children || !children.length) return null;
+
     return (
       <>
-        <StyledList
-          isExpanded={expanded}
-          innerRef={this.listRef}
-          blockHeight={this.computeBlockHeight()}
-          animate={animate}
-        >
-          {children}
-        </StyledList>
-        {children.length > 1 && children.length > showLessItemCount && (
+        <SizeMe monitorHeight render={({ size: { height } }) => this.renderStyledList(height)} />
+
+        {children?.length > 1 && children?.length > showLessItemCount && (
           <StyledViewMoreButtonWrapper>
             <StyledViewMoreButton onClick={() => this.handleClick()}>
               {expanded ? showLessText : showMoreText}
@@ -111,6 +141,7 @@ ViewMore.defaultProps = {
   showLessItemCount: 1,
   showLessText: 'show less',
   showMoreText: 'show more',
+  animationDelay: 400,
 };
 
 ViewMore.propTypes = {
@@ -118,4 +149,7 @@ ViewMore.propTypes = {
   showLessText: PropTypes.string,
   showMoreText: PropTypes.string,
   showLessItemCount: PropTypes.number,
+  animationDelay: PropTypes.number,
 };
+
+export default ViewMore;
